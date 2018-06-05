cffe2f029527a981b460afc7cb595db6e16e26c6
Victor Galan <victor.galan@liferay.com>
2018-06-05 15:45:59 +0200

    Generate two bundles, one for browser and one for node

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,41 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
-export default {
-	input: 'src/main.js',
-	output: {
-		name: 'ApioConsumer',
-		file: 'build/apio-consumer-bundle.js',
-		format: 'umd'
+export default [
+	{
+		input: 'src/main.js',
+		output: {
+			name: 'ApioConsumer',
+			file: 'build/apio-consumer-browser.js',
+			format: 'iife'
+		},
+		plugins: [
+			nodeResolve({
+				browser: true
+			}),
+			commonjs(),
+			babel({
+				plugins: ['transform-runtime'],
+				runtimeHelpers: true,
+				exclude: 'node_modules/**'
+			})
+		]
 	},
-	plugins: [
-		nodeResolve(),
-		commonjs(),
-		babel({
-			plugins: ['transform-runtime'],
-			runtimeHelpers: true,
-			exclude: 'node_modules/**'
-		}),
-	]
-};
+	{
+		input: 'src/main.js',
+		output: {
+			name: 'ApioConsumer',
+			file: 'build/apio-consumer-node.js',
+			format: 'cjs'
+		},
+		plugins: [
+			nodeResolve(),
+			commonjs(),
+			babel({
+				plugins: ['transform-runtime'],
+				runtimeHelpers: true,
+				exclude: 'node_modules/**'
+			})
+		]
+	}
+];
