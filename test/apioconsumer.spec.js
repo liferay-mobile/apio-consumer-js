6627b51f6a3e431f58dd7d73be5537d96361ba3c
Victor Galan <victor.galan@liferay.com>
2018-06-18 10:31:27 +0200

    Create test for operations

--- test/apioconsumer.spec.js
+++ test/apioconsumer.spec.js
@@ -1,19 +1,21 @@
 import ApioConsumer from '../src/consumer/apio-consumer';
-import Operation from '../src/model/operation';
-import httpClientMock from './mocks/client-mock';
+import httpClientMock from './doubles/client-mock';
+import httpClientSpy from './doubles/client-spy';
+import FormData from 'form-data';
 
 import formResponse from './fixtures/response-form';
+
+import {
+	getOperation,
+	getOperationWithForm,
+	getOperationWithMethod,
+} from './objectMother';
 
 describe('Apio consumer getOperation form', () => {
 	it(`should throw a exception if the operation doesn't have a form`, async () => {
 		const consumer = new ApioConsumer();
 
-		const operationWithoutForm = new Operation(
-			'test operation',
-			'',
-			'DELETE',
-			null
-		);
+		const operationWithoutForm = getOperation();
 
 		let errorMessage = 'No error';
 		try {
@@ -35,12 +37,7 @@ describe('Apio consumer getOperation form', () => {
 		const consumer = new ApioConsumer();
 		consumer.client = clientMock;
 
-		const operationWithForm = new Operation(
-			'test operation',
-			'',
-			'PUT',
-			'http://validurl.com'
-		);
+		const operationWithForm = getOperationWithForm();
 
 		const form = await consumer.getOperationForm(operationWithForm);
 
@@ -54,5 +51,73 @@ describe('Apio consumer getOperation form', () => {
 		expect(form.properties[0].required).toBe(false);
 		expect(form.properties[1].name).toBe('birthDate');
 		expect(form.properties[1].required).toBe(true);
+	});
+});
+
+describe('Apio consumer operations', () => {
+	it('should send json body when includeFile is false', async () => {
+		const clientSpy = new httpClientSpy();
+
+		const apioConsumer = new ApioConsumer();
+		apioConsumer.client = clientSpy;
+
+		const operation = getOperation();
+
+		const properties = {
+			name: 'name',
+			surname: 'surname',
+		};
+
+		await apioConsumer.performOperation(operation, properties);
+
+		expect(typeof clientSpy.body).toBe('string');
+	});
+
+	it('should send the correct content type header', async () => {
+		const clientSpy = new httpClientSpy();
+
+		const apioConsumer = new ApioConsumer();
+		apioConsumer.client = clientSpy;
+
+		const operation = getOperation();
+
+		const properties = {
+			name: 'name',
+			surname: 'surname',
+		};
+
+		await apioConsumer.performOperation(operation, properties, true);
+
+		expect(clientSpy.body instanceof FormData).toBeTruthy();
+	});
+
+	it('should send the correct method', async () => {
+		const clientSpy = new httpClientSpy();
+
+		const apioConsumer = new ApioConsumer();
+		apioConsumer.client = clientSpy;
+
+		const method = 'DELETE';
+
+		const operation = getOperationWithMethod(method);
+
+		await apioConsumer.performOperation(operation, {});
+
+		expect(clientSpy.method).toBe(method);
+	});
+
+	it('should send the correct url', async () => {
+		const clientSpy = new httpClientSpy();
+
+		const apioConsumer = new ApioConsumer();
+		apioConsumer.client = clientSpy;
+
+		const targetUrl = 'http://targeturl.com';
+
+		const operation = getOperation(targetUrl);
+
+		await apioConsumer.performOperation(operation, {});
+
+		expect(clientSpy.url).toBe(targetUrl);
 	});
 });
