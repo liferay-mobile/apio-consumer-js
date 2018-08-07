492560b86ac5f2d7a874e478a89038c005bad9dc
Victor Galan <victor.galan@liferay.com>
2018-08-07 17:04:18 +0200

    APIO-230 Pass the consumer config to the spy

--- test/objectMother/get-apio-consumer-with-spy.js
+++ test/objectMother/get-apio-consumer-with-spy.js
@@ -1,9 +1,9 @@
 import ApioConsumer from '../../src/consumer/apio-consumer';
 import httpClientSpy from '../doubles/client-spy';
 
-const getApioConsumerWithSpy = () => {
+const getApioConsumerWithSpy = config => {
 	const clientSpy = new httpClientSpy();
-	const apioConsumer = new ApioConsumer();
+	const apioConsumer = new ApioConsumer(config);
 	apioConsumer.client = clientSpy;
 	return {apioConsumer, clientSpy};
 };
