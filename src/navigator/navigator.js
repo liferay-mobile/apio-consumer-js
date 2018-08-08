5368dff1c4a35b42185a3d01ecc16cbf66607bcb
Victor Galan <victor.galan@liferay.com>
2018-08-08 08:24:02 +0200

    APIO-230 Add missing docs

--- src/navigator/navigator.js
+++ src/navigator/navigator.js
@@ -2,17 +2,24 @@ import ApioConsumer from '../consumer/apio-consumer';
 
 /**
  * Class for navigating accross the API
+ * @review
  */
 export default class Navigator {
 	/**
 	 * Creates a new Navigator
 	 * @param {ApioConsumer} consumer
+	 * @review
 	 */
 	constructor(consumer) {
 		this.consumer = consumer;
 		this.navigationSteps = [];
 	}
 
+	/**
+	 * Add the entry point request to the navigation.
+	 * @return {Navigator} the same instance to allow adding more navigation steps.
+	 * @review
+	 */
 	getEntryPoint() {
 		const url = this.getEntryPointUrl();
 
@@ -23,6 +30,10 @@ export default class Navigator {
 		return this;
 	}
 
+	/**
+	 * Get the url of the entry point
+	 * @review
+	 */
 	getEntryPointUrl() {
 		return this.consumer.config + '/o/api';
 	}
@@ -39,10 +50,20 @@ export default class Navigator {
 		return this;
 	}
 
+	/**
+	 * Get the final result after executing all steps
+	 * @review
+	 */
 	get() {
 		return this.executeSteps(this.navigationSteps);
 	}
 
+	/**
+	 * Execute all steps
+	 * @param {Array} steps
+	 * @return {object}
+	 * @review
+	 */
 	executeSteps(steps) {
 		return steps.reduce(async (acc, step) => step(await acc), null);
 	}
